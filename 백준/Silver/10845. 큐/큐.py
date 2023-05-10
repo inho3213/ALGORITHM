a4599e8f15795dddcea4ecdb88462bbb4f31427a
INHO <61544239+inho3213@users.noreply.github.com>
2023-05-11 08:05:53 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+
+q = deque()
+N = int(sys.stdin.readline())
+for _ in range(N):
+  command = sys.stdin.readline().split()
+  if command[0] == 'push':
+    q.append(command[1])
+  elif command[0] == 'pop':
+    if q:
+      print(q.popleft())
+    else:
+      print(-1)
+  elif command[0] == 'size':
+    print(len(q))
+  elif command[0] == 'empty':
+    if q:
+      print(0)
+    else:
+      print(1)
+  elif command[0] == 'front':
+    if q:
+      print(q[0])
+    else:
+      print(-1)
+  elif command[0] == 'back':
+    if q:
+      print(q[len(q)-1])
+    else:
+      print(-1)
